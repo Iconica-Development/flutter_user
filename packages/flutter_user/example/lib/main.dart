ab112f78f436d758f3deebf17e83a3ef9a0b71f4
Freek van de Ven <freek@freekvandeven.nl>
2025-04-17 12:59:08 +0200

    chore: cleanup flutter_user example app

--- packages/flutter_user/example/lib/main.dart
+++ packages/flutter_user/example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:example/theme.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_user/flutter_user.dart';
+import "package:example/theme.dart";
+import "package:flutter/material.dart";
+import "package:flutter_user/flutter_user.dart";
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -10,47 +10,42 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "flutter_user Example",
-      theme: theme,
-      home: FlutterUserNavigatorUserstory(
-        afterLoginScreen: const Home(),
-        options: FlutterUserOptions(
-          loginOptions: const LoginOptions(
-            biometricsOptions: LoginBiometricsOptions(
-              loginWithBiometrics: true,
+  Widget build(BuildContext context) => MaterialApp(
+        title: "flutter_user Example",
+        theme: theme,
+        home: FlutterUserNavigatorUserstory(
+          afterLoginScreen: const Home(),
+          options: FlutterUserOptions(
+            loginOptions: const LoginOptions(
+              biometricsOptions: LoginBiometricsOptions(
+                loginWithBiometrics: true,
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const FlutterUserNavigatorUserstory(
-                    afterLoginScreen: Home(),
-                  ),
-                ));
-          },
-          child: const Text("Logout"),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
         ),
-      ),
-    );
-  }
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const FlutterUserNavigatorUserstory(
+                  afterLoginScreen: Home(),
+                ),
+              ),
+            ),
+            child: const Text("Logout"),
+          ),
+        ),
+      );
 }
